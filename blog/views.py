b12d1c1094a3275b8ccbbef0ab2468cf34126bf2
hlorofilka <hlorofilk@mail.ru>
2019-07-04 15:39:54 +0200

    The third lesson

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+    'author': 'CoreyMS',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'July 04, 2019'
+    },
+    {
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'July 05, 2019'
+    }
+
+]
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+          'posts':posts
+          }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
