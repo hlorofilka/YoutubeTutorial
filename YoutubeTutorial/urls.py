06ccb0ccb52ff3daea76c22a50896de8110d12ac
hlorofilka <hlorofilk@mail.ru>
2019-07-26 16:47:45 +0200

    The tenth lesson

--- YoutubeTutorial/urls.py
+++ YoutubeTutorial/urls.py
@@ -1,7 +1,6 @@
-"""YoutubeTutorial URL Configuration
-
+"""django_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
